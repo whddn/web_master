8878b3a42dd1cdcb7307bc887739d188ed7d458d
whddn <ghjkl133120@naver.com>
2024-11-15 17:46:37 +0900

    feat: array

---
+++ js/1115/arrayExe3.js
@@ -0,0 +1,32 @@
+/*
+let friendAry = [
+  {name: "홍길동", age: 20, weight: 56.7},
+  {name: "김민기", age: 21, weight: 93.7},
+  {name: "박철순", age: 22, weight: 79.7},
+  {name: "이만수", age: 23, weight: 88.7}
+]
+
+let temp = friendAry.filter((friend) =>{
+  if(friend.weight > 60){
+    console.log(friend)
+    return true
+  }
+  return false
+})
+console.log(temp)
+*/
+
+/*
+empAry => 사원중에서 성별: 여자, 급여: 5000이상
+=> femaleAry 저장
+*/
+
+// let femaleAry = empAry.filter((friend)=>
+//   friend.gender == 'Female' && friend.salary >= 5000)
+  // if(friend.gender == 'Female' && friend.salary >= 5000){
+  //   return true
+  // }
+  // return false
+
+// console.log(femaleAry);
+
