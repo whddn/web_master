b6febb35dc7672a09ab3407a72fb7acbdf193af7
whddn <ghjkl133120@naver.com>
2024-11-25 11:17:53 +0900

    장바구니 기능 추가

--- project/login/login.js
+++ project/login/login.js
@@ -7,10 +7,12 @@ document.querySelector('button.submit-btn').addEventListener('click', e => {
 
   let loginInfo = memberData.filter(member => member.id == id && member.pw == pw)
 
+  
   if (loginInfo.length) { // id, pw 동일한 정보가 있다는 의미
     localStorage.setItem('logId', id)
     localStorage.setItem('logName', loginInfo[0].name)
     location.href = '/project/main/main.html'
+    
   } else {
     alert('입력정보를 확인하세요!')
   }
