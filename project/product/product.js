0f36e4b5ffa69d5479088bd89d41ece9e991ebec
whddn <ghjkl133120@naver.com>
2024-11-25 10:24:08 +0900

    장바구니 추가 알림

--- project/product/product.js
+++ project/product/product.js
@@ -5,15 +5,21 @@ let cartData = JSON.parse(localStorage.getItem('cartData')) // 장바구니목�
 productData.forEach(product => {
   console.log(product)
   let template = `<div class="col" data-pcode="${product.pcode}">
-    <div class="card" style="width: 20rem;">
+    <div class="card" style="width: 18rem;">
       <img src="../images/${product.images}" height="350px" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">${product.pcode}</h5>
-        <p class="card-text">${product.pname}<br>${product.price}원</p>
-        <a href="#" class="btn btn-success">장바구니 담기</a>
+        <img src="../images/today.svg" height="16px">
+        <h4 class="card-title">${product.pname}</h5>
+        <p class= "card-text" >₩${product.price}</p>
+        <a href="#" class="btn btn-outline-success">장바구니 담기🎁</a>
       </div>
     </div>
   </div>`
   document.querySelector('div.row').insertAdjacentHTML("beforeend", template)
+
+})
+document.querySelector('.btn.btn-outline-success').addEventListener('click', e =>{
+  alert("상품을 장바구니에 담았습니다")
+  
 })
 
