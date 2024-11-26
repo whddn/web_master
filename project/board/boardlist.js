6aa83c23d53451a3e8af31725ecd7c465079d7c8
whddn <ghjkl133120@naver.com>
2024-11-26 16:58:16 +0900

    전체수정

--- project/board/boardlist.js
+++ project/board/boardlist.js
@@ -1,3 +1,5 @@
 let boardList = [
-  {file:'images.jpg', productname:'검정 원피스', review: '상품이 마음에 들어요', name:'이종우', date:'2024-11-26'  }
+  {file:'/images/images.jpg', productname:'검정 원피스', review: '늘 잘 입고 있어요(참고로 남잡니다)', name:'김*홍', date:'2024-11-27'  },
+  {file:'images.jpg', productname:'미니 스커트', review: '상품이 마음에 들어요(명절이라 샀는데 최고의 선택)', name:'***우', date:'2024-11-11'  },
+  {file:'', productname:'청치마', review: '제가 원래 77입는데 이건 66도 잘 맞네요~ㅎㅎ', name:'*상연', date:'2024-09-05'  }
 ]
